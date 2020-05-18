83aeb8803f793f21f0d917051946508c4e5b1d05
Schneider Erhardt <chrome_erhardt@yahoo.de>
2020-05-18 13:03:05 +0300

    Users,Projects,Allocations

--- WebProjectManager/Data/Allocations.cs
+++ WebProjectManager/Data/Allocations.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,18 @@ namespace WebProjectManager.Data
 {
     public class Allocations
     {
+        [Key]
+        [Column(Order = 1)]
         public int UsersId { get; set; }
-
+        [Key]
+        [Column(Order = 2)]
         public int ProjectsId { get; set; }
+
+        [ForeignKey("UsersId")]
+        public Users Users { get; set; }
+
+        [ForeignKey("ProjectsId")]
+        public Projects Projects { get; set; }
 
     }
 }
---
+++ WebProjectManager/Data/IProjectManagerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProjectManager.Data
+{
+    public interface IProjectManagerRepository
+    {
+        IQueryable<Users> GetUsers();
+        IQueryable<Projects> GetProjects();
+        IQueryable<Allocations> GetAllocation();
+    }
+}
